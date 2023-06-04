c547223259e18d39e9214812b43d506e2f428f13
Peter McKasty <pmck25@yahoo.com>
2023-06-04 17:00:23 -0400

    added necessary module packages and inquirer prompt method to allow for user answers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -79,7 +80,18 @@ function writeToFile(answers) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+inquirer.prompt(questions).then((answers) => {
+    // Generate README content based on the answers
+    const readmeContent = generateReadme(answers);
+  
+    // Write the generated README content to a file
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md file generated successfully!');
+      }
+    });
+  });
 
-// Function call to initialize app
-init();
+
